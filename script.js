401223c9aefcc5ccfa43358b494eea8c8f5ae083
Djazz22 <106832875+Djazz22@users.noreply.github.com>
2024-06-29 21:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    // Check if either of the input strings is empty
+    if (str1 === '' || str2 === '') {
+        return '';
+    }
+    
+    // Return the initial letters of the input strings
+    return str1.charAt(0) + str2.charAt(0);
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+document.getElementById('shortcutForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var inputStr1 = document.getElementById('inputStr1').value;
+    var inputStr2 = document.getElementById('inputStr2').value;
+    var result = shortcut(inputStr1, inputStr2);
+
+    document.getElementById('result').innerText = 'Shortcut: ' + result;
+});
